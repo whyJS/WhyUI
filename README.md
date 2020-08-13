5b76d54593e6d17bf2303706fe85bd87eb0e511b
why <why3935308@163.com>
2020-08-13 14:51:00 +0800

    asd

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# WhyUI
-# 前端UI框架
+# why-ui
+
+## Project setup
+
+```
+npm install
+```
+
+### Compiles and hot-'eloa'sfor development
+
+```
+npm run serve
+```
+
+''
+
+### Compiles and'minifies for prod'ction
+
+```''
+npm run build
+```
+
+### Lints and fixes files
+
+```''
+npm run lint''
+```
+
+''
+
+### Customize 'onfiguration'
+
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ vue.config.js
@@ -0,0 +1,28 @@
+/*
+ * @Description:
+ * @Author: whyjs
+ * @Date: 2020-08-13 13:29:09
+ * @LastEditTime: 2020-08-13 13:41:45
+ * @LastEditors: whyjs
+ */
+const path = require('path')
+module.exports = {
+  pages: {
+    index: {
+      entry: 'src/main.js',
+      template: 'public/index.html',
+      filename: 'index.html'
+    }
+  },
+  chainWebpack: config => {
+    config.module
+      .rule('js')
+      .include.add(path.resolve(__dirname, 'packages'))
+      .end()
+      .use('babel')
+      .loader('babel-loader')
+      .tap(options => {
+        return options
+      })
+  }
+}
